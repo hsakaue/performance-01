80c7c306d884da933424935e3ee1ccbe372b7ae4
hsakaue <hsakaue.sunarch@gmail.com>
2021-10-02 12:20:45 +0000

    test

--- src/main/java/com/performance/domain/service/PerformanceService.java
+++ src/main/java/com/performance/domain/service/PerformanceService.java
@@ -74,7 +74,7 @@ public class PerformanceService {
                 //データ内容をコンソールに表示する
                 // log.info("-------------------------------");
                 //データ件数を表示
-                // log.debug("データ書き込み" + i + "件目");
+                log.info("データ書き込み" + i + "件目");
 
                 //配列の中身を順位表示する。列数(=列名を格納した配列の要素数)分繰り返す
                 // log.debug("ユーザー性:" + data[1]);
@@ -128,7 +128,7 @@ public class PerformanceService {
                 // log.info("-------------------------------");
 
                 //データ件数を表示
-                // log.debug("データ読み込み" + i + "件目");
+                log.info("データ読み込み" + i + "件目");
                 
                 csvFile.add(readLine);
             }
