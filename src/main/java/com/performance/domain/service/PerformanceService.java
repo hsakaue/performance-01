60fb3e01751675777b0e1a66dd22a1364e243df4
hsakaue <hsakaue.sunarch@gmail.com>
2021-10-09 08:15:43 +0000

    test

--- src/main/java/com/performance/domain/service/PerformanceService.java
+++ src/main/java/com/performance/domain/service/PerformanceService.java
@@ -84,10 +84,10 @@ public class PerformanceService {
             while ((readLine = br.readLine()) != null) {
                 i++;
                 //データ内容をコンソールに表示する
-                log.info("-------------------------------");
+                // log.info("-------------------------------");
 
                 //データ件数を表示
-                log.info("データ読み込み" + i + "件目");
+                // log.info("データ読み込み" + i + "件目");
                 
                 csvFile.add(readLine);
             }
@@ -109,9 +109,9 @@ public class PerformanceService {
                 String[] data = line.split(",", -1);
                 
                 //データ内容をコンソールに表示する
-                log.info("-------------------------------");
+                // log.info("-------------------------------");
                 //データ件数を表示
-                log.info("データ書き込み" + i + "件目");
+                if (i % 100 == 0) log.info("データ書き込み" + i + "件目");
                 //配列の中身を順位表示する。列数(=列名を格納した配列の要素数)分繰り返す
                 log.debug("ユーザー姓:" + data[1]);
                 log.debug("出身都道府県:" + data[2]);
