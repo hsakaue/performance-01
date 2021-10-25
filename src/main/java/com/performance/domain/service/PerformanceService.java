b3918c260c41468e3704f12a85b01b7f3e8a334c
hsakaue <hsakaue.sunarch@gmail.com>
2021-10-25 12:14:08 +0000

    update

--- src/main/java/com/performance/domain/service/PerformanceService.java
+++ src/main/java/com/performance/domain/service/PerformanceService.java
@@ -10,13 +10,15 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import com.performance.domain.dao.UserInfoDao;
-import com.performance.domain.entity.UserInfo;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import com.performance.domain.dao.UserDao;
+import com.performance.domain.entity.UserHobby;
+import com.performance.domain.entity.UserInfo;
+import com.performance.domain.entity.UserMaster;
 
 @Service
 public class PerformanceService {
@@ -27,14 +29,14 @@ public class PerformanceService {
 
     private GoogleApiService googleService;
 
-    private UserInfoDao userInfoDao;
+    private UserDao userDao;
     
     private Map<String, Long> resultMap = new HashMap<String, Long>();
     private Map<String, Boolean> assertionResultMap = new HashMap<String, Boolean>();
 
-    public PerformanceService(GoogleApiService googleService, UserInfoDao userInfoDao) {
+    public PerformanceService(GoogleApiService googleService, UserDao userDao) {
         this.googleService = googleService;
-        this.userInfoDao = userInfoDao;
+        this.userDao = userDao;
     }
 
     @Async("perfomanceExecutor")
@@ -43,11 +45,11 @@ public class PerformanceService {
         resultMap.clear();
         resultMap.put(uuid, null);
 
-        truncateTable();
+        
 
         Long start = System.currentTimeMillis();
 
-        List<UserInfo> matchingUserList = uploadExecute();
+        List<UserMaster> matchingUserList = uploadExecute();
 
         Long end = System.currentTimeMillis();
         Long executeTime = end - start;
@@ -57,7 +59,8 @@ public class PerformanceService {
         Boolean assertionResult = assertion(matchingUserList);
         assertionResultMap.put(uuid, assertionResult);
         
-        if(MEASURE_FLAG_ON.equals(measureFlag)) {
+        // 計測実施かつアサーションが成功している場合のみ送る
+        if(MEASURE_FLAG_ON.equals(measureFlag) && assertionResult) {
             try {
                 googleService.execute(executeTime);
             } catch (Exception e) {
@@ -66,8 +69,12 @@ public class PerformanceService {
         }
         return;
     }
-    
-    public List<UserInfo> uploadExecute() {
+    public List<UserMaster> uploadExecute() {
+        // テーブル情報を空にする
+        /** 変更不可 **/
+        truncateTable();
+        /** 変更不可 **/
+        
         // CSVを取得・CSVファイルをDBに登録する
         //ファイル読み込みで使用する3つのクラス
         FileReader fr = null;
@@ -78,23 +85,23 @@ public class PerformanceService {
             //読み込みファイルのインスタンス生成
             //ファイル名を指定する
             fr = new FileReader(new File("data/userInfo.csv"));
-            // InputStreamReader is = new InputStreamReader(new FileInputStream(new File("data/userInfo.csv")), StandardCharsets.UTF_8);
             br = new BufferedReader(fr);
+            
 
             //読み込み行
             String readLine;
 
             //読み込み行数の管理
-            // int i = 0;
+            int i = 0;
 
             //1行ずつ読み込みを行う
             while ((readLine = br.readLine()) != null) {
-                // i++;
+                i++;
                 //データ内容をコンソールに表示する
-                // log.info("-------------------------------");
+                log.info("-------------------------------");
 
                 //データ件数を表示
-                // log.info("データ読み込み" + i + "件目");
+                log.info("データ読み込み" + i + "件目");
                 
                 csvFile.add(readLine);
             }
@@ -108,130 +115,154 @@ public class PerformanceService {
         }
 
         try {
-            // int i = 0;
-            List<UserInfo> userInfoList = new ArrayList<>();
-            Pattern ptn = Pattern.compile(".*新潟県,上越市.*");
+            int i = 0;
             for(String line : csvFile) {
                 //カンマで分割した内容を配列に格納する
-                Matcher matcher = ptn.matcher(line);
-                if (matcher.find()) {
-                    String[] data = line.split(",", -1);
-                    UserInfo userInfo = new UserInfo();
-                    userInfo.setLastName(data[0]);
-                    userInfo.setFirstName(data[1]);
-                    userInfo.setPrefectures(data[2]);
-                    userInfo.setCity(data[3]);
-                    userInfo.setBloodType(data[4]);
-                    userInfo.setHobby1(data[5]);
-                    userInfo.setHobby2(data[6]);
-                    userInfo.setHobby3(data[7]);
-                    userInfo.setHobby4(data[8]);
-                    userInfo.setHobby5(data[9]);
-                    userInfoList.add(userInfo);
-                }
-
-                // String[] data = line.split(",", -1);
+                String[] data = line.split(",", -1);
                 
                 //データ内容をコンソールに表示する
-                // log.info("-------------------------------");
+                log.info("-------------------------------");
                 //データ件数を表示
                 //配列の中身を順位表示する。列数(=列名を格納した配列の要素数)分繰り返す
-                // log.debug("ユーザー姓:" + data[1]);
-                // log.debug("出身都道府県:" + data[2]);
-                // log.debug("ユーザー名:" + data[0]);
-                // log.debug("出身市区町村:" + data[3]);
-                // log.debug("血液型:" + data[4]);
-                // log.debug("趣味1:" + data[5]);
-                // log.debug("趣味2:" + data[6]);
-                // log.debug("趣味3:" + data[7]);
-                // log.debug("趣味4:" + data[8]);
-                // log.debug("趣味5:" + data[9]);
-                // UserInfo userInfo = new UserInfo();
+                log.debug("ユーザー姓:" + data[1]);
+                log.debug("出身都道府県:" + data[2]);
+                log.debug("ユーザー名:" + data[0]);
+                log.debug("出身市区町村:" + data[3]);
+                log.debug("血液型:" + data[4]);
+                log.debug("趣味1:" + data[5]);
+                log.debug("趣味2:" + data[6]);
+                log.debug("趣味3:" + data[7]);
+                log.debug("趣味4:" + data[8]);
+                log.debug("趣味5:" + data[9]);
+                UserInfo userInfo = new UserInfo();
+                UserHobby userHobby = new UserHobby();
 
-                // userInfo.setLastName(data[0]);
-                // userInfo.setFirstName(data[1]);
-                // userInfo.setPrefectures(data[2]);
-                // userInfo.setCity(data[3]);
-                // userInfo.setBloodType(data[4]);
-                // userInfo.setHobby1(data[5]);
-                // userInfo.setHobby2(data[6]);
-                // userInfo.setHobby3(data[7]);
-                // userInfo.setHobby4(data[8]);
-                // userInfo.setHobby5(data[9]);
-                // // 特定の件のみインサートするようにする
-                // if("新潟県上越市".equals(userInfo.getPrefectures() + userInfo.getCity())) {
-                //     // 行数のインクリメント
-                //     // i++;
-                //     // log.info("データ書き込み" + i + "件目");
-                //     // userInfoDao.insert(userInfo);
-                //     userInfoList.add(userInfo);
-                // }
+                userInfo.setLastName(data[0]);
+                userInfo.setFirstName(data[1]);
+                userInfo.setPrefectures(data[2]);
+                userInfo.setCity(data[3]);
+                userInfo.setBloodType(data[4]);
+                userHobby.setHobby1(data[5]);
+                userHobby.setHobby2(data[6]);
+                userHobby.setHobby3(data[7]);
+                userHobby.setHobby4(data[8]);
+                userHobby.setHobby5(data[9]);
+                // 特定の件のみインサートするようにする
+                Pattern pattern = Pattern.compile(".新潟県,上越市.");
+                Matcher matcher = pattern.matcher(line);
+                if(matcher.find()) {
+                    // 行数のインクリメント
+                    i++;
+                    log.info("データ書き込み" + i + "件目");
+                    userDao.insertUserInfo(userInfo);
+                    Long id = userDao.selectId(userInfo);
+                    userHobby.setId(id);
+                    userDao.insertUserHobby(userHobby);
+                }
             }
-            userInfoDao.insert(userInfoList);
+
         } catch (Exception e) {
             log.info("csv read error", e);
         }
-
-        UserInfo targetUser = userInfoDao.getTargetUser();
+        // 対象情報取得
+        UserInfo targetUserInfo = userDao.getTargetUserInfo();
+        UserHobby targetUserHobby = userDao.getTargetUserHobby(targetUserInfo);
+        UserMaster targetUserMaster = new UserMaster();
+        
+        targetUserMaster.setId(targetUserInfo.getId());
+        targetUserMaster.setLastName(targetUserInfo.getLastName());
+        targetUserMaster.setFirstName(targetUserInfo.getFirstName());
+        targetUserMaster.setPrefectures(targetUserInfo.getPrefectures());
+        targetUserMaster.setCity(targetUserInfo.getCity());
+        targetUserMaster.setBloodType(targetUserInfo.getBloodType());
+        targetUserMaster.setHobby1(targetUserHobby.getHobby1());
+        targetUserMaster.setHobby2(targetUserHobby.getHobby2());
+        targetUserMaster.setHobby3(targetUserHobby.getHobby3());
+        targetUserMaster.setHobby4(targetUserHobby.getHobby4());
+        targetUserMaster.setHobby5(targetUserHobby.getHobby5());
         
         // DBから検索する
-        List<UserInfo> userInfoList = userInfoDao.search();
+        List<UserInfo> userInfoList = userDao.searchUserInfo();
+        List<UserHobby> userHobbyList = userDao.searchUserHobby(targetUserHobby);
         
-        List<UserInfo> bloodMatchingUserList = new ArrayList<UserInfo>();
+        List<UserMaster> userMasterList = new ArrayList<UserMaster>();
+        
+        for(int i = 0; i < userInfoList.size(); i++) {
+            UserMaster userMaster = new UserMaster();
+            userMaster.setId(userInfoList.get(i).getId());
+            userMaster.setLastName(userInfoList.get(i).getLastName());
+            userMaster.setFirstName(userInfoList.get(i).getFirstName());
+            userMaster.setPrefectures(userInfoList.get(i).getPrefectures());
+            userMaster.setCity(userInfoList.get(i).getCity());
+            userMaster.setBloodType(userInfoList.get(i).getBloodType());
+            for(int j = 0; j < userHobbyList.size(); j++) {
+                if(userMaster.getId().equals(userHobbyList.get(j).getId())) {
+                    userMaster.setHobby1(userHobbyList.get(j).getHobby1());
+                    userMaster.setHobby2(userHobbyList.get(j).getHobby2());
+                    userMaster.setHobby3(userHobbyList.get(j).getHobby3());
+                    userMaster.setHobby4(userHobbyList.get(j).getHobby4());
+                    userMaster.setHobby5(userHobbyList.get(j).getHobby5());
+                    break;
+                }
+            }
+            userMasterList.add(userMaster);
+        }
+        
+        List<UserMaster> bloodMatchingUserList = new ArrayList<UserMaster>();
         // 同じ血液型ユーザー
-        for(UserInfo user : userInfoList) {
-            if(user.getBloodType().equals(targetUser.getBloodType())) {
+        for(UserMaster user : userMasterList) {
+            if(user.getBloodType().equals(targetUserMaster.getBloodType())) {
                 bloodMatchingUserList.add(user);
             }
         }
-        List<UserInfo> matchingUserList = new ArrayList<UserInfo>();
+        List<UserMaster> matchingUserList = new ArrayList<UserMaster>();
         // 趣味1に同じ趣味を持っているユーザー
-        for(UserInfo user : bloodMatchingUserList) {
-            if(user.getHobby1().equals(targetUser.getHobby1()) || user.getHobby1().equals(targetUser.getHobby2()) || user.getHobby1().equals(targetUser.getHobby3()) || user.getHobby1().equals(targetUser.getHobby4()) || user.getHobby1().equals(targetUser.getHobby5())) {
+        for(UserMaster user : bloodMatchingUserList) {
+            if(user.getHobby1().equals(targetUserMaster.getHobby1()) || user.getHobby1().equals(targetUserMaster.getHobby2()) || user.getHobby1().equals(targetUserMaster.getHobby3()) || user.getHobby1().equals(targetUserMaster.getHobby4()) || user.getHobby1().equals(targetUserMaster.getHobby5())) {
                 if(!matchingUserList.contains(user)) {
                     matchingUserList.add(user);
                 }
             }
         }
         // 趣味2に同じ趣味を持っているユーザー
-        for(UserInfo user : bloodMatchingUserList) {
-            if(user.getHobby2().equals(targetUser.getHobby1()) || user.getHobby1().equals(targetUser.getHobby2()) || user.getHobby1().equals(targetUser.getHobby3()) || user.getHobby1().equals(targetUser.getHobby4()) || user.getHobby1().equals(targetUser.getHobby5())) {
+        for(UserMaster user : bloodMatchingUserList) {
+            if(user.getHobby2().equals(targetUserMaster.getHobby1()) || user.getHobby1().equals(targetUserMaster.getHobby2()) || user.getHobby1().equals(targetUserMaster.getHobby3()) || user.getHobby1().equals(targetUserMaster.getHobby4()) || user.getHobby1().equals(targetUserMaster.getHobby5())) {
                 if(!matchingUserList.contains(user)) {
                     matchingUserList.add(user);
                 }
             }
         }
         // 趣味3に同じ趣味を持っているユーザー
-        for(UserInfo user : bloodMatchingUserList) {
-            if(user.getHobby3().equals(targetUser.getHobby1()) || user.getHobby1().equals(targetUser.getHobby2()) || user.getHobby1().equals(targetUser.getHobby3()) || user.getHobby1().equals(targetUser.getHobby4()) || user.getHobby1().equals(targetUser.getHobby5())) {
+        for(UserMaster user : bloodMatchingUserList) {
+            if(user.getHobby3().equals(targetUserMaster.getHobby1()) || user.getHobby1().equals(targetUserMaster.getHobby2()) || user.getHobby1().equals(targetUserMaster.getHobby3()) || user.getHobby1().equals(targetUserMaster.getHobby4()) || user.getHobby1().equals(targetUserMaster.getHobby5())) {
                 if(!matchingUserList.contains(user)) {
                     matchingUserList.add(user);
                 }
             }
         }
         // 趣味4に同じ趣味を持っているユーザー
-        for(UserInfo user : bloodMatchingUserList) {
-            if(user.getHobby4().equals(targetUser.getHobby1()) || user.getHobby1().equals(targetUser.getHobby2()) || user.getHobby1().equals(targetUser.getHobby3()) || user.getHobby1().equals(targetUser.getHobby4()) || user.getHobby1().equals(targetUser.getHobby5())) {
+        for(UserMaster user : bloodMatchingUserList) {
+            if(user.getHobby4().equals(targetUserMaster.getHobby1()) || user.getHobby1().equals(targetUserMaster.getHobby2()) || user.getHobby1().equals(targetUserMaster.getHobby3()) || user.getHobby1().equals(targetUserMaster.getHobby4()) || user.getHobby1().equals(targetUserMaster.getHobby5())) {
                 if(!matchingUserList.contains(user)) {
                     matchingUserList.add(user);
                 }
             }
         }
         // 趣味5に同じ趣味を持っているユーザー
-        for(UserInfo user : bloodMatchingUserList) {
-            if(user.getHobby5().equals(targetUser.getHobby1()) || user.getHobby1().equals(targetUser.getHobby2()) || user.getHobby1().equals(targetUser.getHobby3()) || user.getHobby1().equals(targetUser.getHobby4()) || user.getHobby1().equals(targetUser.getHobby5())) {
+        for(UserMaster user : bloodMatchingUserList) {
+            if(user.getHobby5().equals(targetUserMaster.getHobby1()) || user.getHobby1().equals(targetUserMaster.getHobby2()) || user.getHobby1().equals(targetUserMaster.getHobby3()) || user.getHobby1().equals(targetUserMaster.getHobby4()) || user.getHobby1().equals(targetUserMaster.getHobby5())) {
                 if(!matchingUserList.contains(user)) {
                     matchingUserList.add(user);
                 }
             }
         }
-        // List<UserInfo> matchingUserList = userInfoDao.search2(targetUser);
         return matchingUserList;
     }
 
     
     public void truncateTable() {
-        userInfoDao.truncate();
+        userDao.truncateUserInfo();
+        userDao.truncateUserHobby();
     }
 
     public Long referenceExecuteTime(String uuid) {
@@ -255,10 +286,10 @@ public class PerformanceService {
         return uuid;
     }
 
-    private Boolean assertion(List<UserInfo> matchingUserList) {
+    private Boolean assertion(List<UserMaster> matchingUserList) {
         Boolean assertionResult = true;
         
-        int count = userInfoDao.searchCount();
+        int count = userDao.searchCount();
         
         if(count != 10000) {
             return false;
@@ -296,21 +327,21 @@ public class PerformanceService {
         }
         for(String line : csvFile) {
             boolean exsits = false;
-            UserInfo userInfo = new UserInfo();
+            UserMaster userMaster = new UserMaster();
             String[] data = line.split(",", -1);
 
-            userInfo.setLastName(data[0]);
-            userInfo.setFirstName(data[1]);
-            userInfo.setPrefectures(data[2]);
-            userInfo.setCity(data[3]);
-            userInfo.setBloodType(data[4]);
-            userInfo.setHobby1(data[5]);
-            userInfo.setHobby2(data[6]);
-            userInfo.setHobby3(data[7]);
-            userInfo.setHobby4(data[8]);
-            userInfo.setHobby5(data[9]);
-            for(UserInfo user : matchingUserList) {
-                if(user.toString().equals(userInfo.toString())) {
+            userMaster.setLastName(data[0]);
+            userMaster.setFirstName(data[1]);
+            userMaster.setPrefectures(data[2]);
+            userMaster.setCity(data[3]);
+            userMaster.setBloodType(data[4]);
+            userMaster.setHobby1(data[5]);
+            userMaster.setHobby2(data[6]);
+            userMaster.setHobby3(data[7]);
+            userMaster.setHobby4(data[8]);
+            userMaster.setHobby5(data[9]);
+            for(UserMaster user : matchingUserList) {
+                if(user.toString().equals(userMaster.toString())) {
                     exsits = true;
                     break;
                 }
@@ -319,6 +350,7 @@ public class PerformanceService {
                 assertionResult = false;
             }
         }
+        truncateTable();
         return assertionResult;
     }
 
